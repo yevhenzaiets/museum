d8ccdfd44631a2ed570c7444489c9878f7bf6967
My name <myEmail@example.com>
2021-12-02 20:56:05 +0200

    feat: add slider

---
+++ slider.js
@@ -0,0 +1,58 @@
+const prev = document.getElementById('prev'),
+      next = document.getElementById('next'),
+      slides = document.querySelectorAll('.slide'),
+      bullets = document.querySelectorAll('.bullet'),
+      fraction = document.getElementById('number');
+
+
+const activeSlide = s => {
+    for(let slide of slides) {
+        slide.classList.remove('active');
+    }
+    slides[s].classList.add('active');
+    fraction.textContent = slides[s].getAttribute('data-number');
+}
+
+const activeBulletSlide = s => {
+    for(let bullet of bullets) {
+        bullet.classList.remove('active');
+    }
+    bullets[s].classList.add('active');
+}
+
+const showCurrentSlide = ind => {
+    activeSlide(ind);
+    activeBulletSlide(ind);
+}
+
+indexSlide = 0;
+
+const nextSlide = () => {
+    if(indexSlide == slides.length - 1) {
+        indexSlide = 0;
+        showCurrentSlide(indexSlide);
+    }else{
+        indexSlide++;
+        showCurrentSlide(indexSlide);
+    }
+}
+
+const prevSlide = () => {
+    if(indexSlide == 0) {
+        indexSlide = slides.length - 1;
+        showCurrentSlide(indexSlide);
+    }else{
+        indexSlide--;
+        showCurrentSlide(indexSlide);
+    }
+}
+
+bullets.forEach((item, bulletIndex) => {
+    item.addEventListener("click", () => {
+        indexSlide = bulletIndex;
+        showCurrentSlide(indexSlide);
+    })
+})
+
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
