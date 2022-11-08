e21dbf470e322249091516c0d6884b65cb509527
My name <myEmail@example.com>
2022-11-08 14:30:51 +0200

    implement correct the menu appears for 768px screen

--- slider.js
+++ slider.js
@@ -56,8 +56,7 @@ bullets.forEach((item, bulletIndex) => {
 next.addEventListener("click", nextSlide);
 prev.addEventListener("click", prevSlide);
 
-
-// show burger menu for 1024px
+// menu for 1024px
 const menu = document.querySelector(".burger-menu");
 const hiddenContent = document.querySelectorAll(".slider-title");
 const menuList = document.querySelector(".menu-list");
@@ -100,10 +99,62 @@ menu.addEventListener("click", () => {
             img.src = `assets/png/Vector-.png`;
             img.alt = `vector`;
             vector.append(img);
-        }        
+        }    
     }
+    checkWidth();
 });
 
+// menu for 768px
+function checkWidth () {
+    
+    let paginator = document.querySelector(".paginator");
+    const menuFooterWrapper = document.querySelector(".mobile-content");
+    const menuFooter = document.createElement('div');
+    const footerNav = document.querySelector(".social-networks-contacts");
+    const basicSlide = document.querySelector(".slide.active");
+
+    if (window.screen.width <= 768) {
+        /// hide padinator
+        paginator.style.display = 'none';
+        /// add center elem menu
+        menuFooter.classList.add("menu-footer");
+        /// add photos 
+        const menuImg1 = document.createElement('img');
+        menuImg1.src = `assets/png/bm-1.png`;
+        menuImg1.alt = `menu1`;
+
+        const menuImg2 = document.createElement('img');
+        menuImg2.src = `assets/png/bm-2.png`;
+        menuImg2.alt = `menu2`;
+
+        const menuImg3 = document.createElement('img');
+        menuImg3.src = `assets/png/bm-3.png`;
+        menuImg3.alt = `menu3`;
+        /// add style bottom picture
+        menuImg2.style.paddingTop = "20px";
+        menuImg3.style.paddingTop = "20px";
+        menuImg3.style.paddingLeft = "20px";
+        /// add imgs to page
+        menuFooter.append(menuImg1, menuImg2, menuImg3);
+        menuFooterWrapper.appendChild(menuFooter);
+        /// change height active slide
+        basicSlide.style.minHeight = "860px";
+        /// get footerNav style
+        footerNav.classList.add("to-top-menu");
+    }
+    
+    if(menu.classList.contains("burger-menu")) {
+        /// set up heiht for main slide and show paginator
+        basicSlide.style.minHeight = "760px";
+        paginator.style.display = 'flex';
+        /// remove child  main menu block
+        menuFooter.remove();
+        menuFooterWrapper.removeChild(menuFooterWrapper.firstElementChild);
+        /// remove class which show footer elements
+        footerNav.classList.remove("to-top-menu");
+        menuFooter.classList.remove("menu-footer");
+    }
+}
 
 
 
